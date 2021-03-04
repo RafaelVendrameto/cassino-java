97c5bd62eb558f90ab23c859a136c11cbe567f29
RafaelVendrameto <rafaelvendrameto@gmail.com>
2021-03-04 15:21:20 -0300

    model de jogador

---
+++ src/cassino/Jogador.java
@@ -0,0 +1,37 @@
+package cassino;
+
+public class Jogador {
+
+    private String nome;
+    private int saldo;
+    private int numeroSorteado;
+
+    public Jogador(String nome, int saldo) {
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getNumeroSorteado() {
+        return numeroSorteado;
+    }
+
+    public void setNumeroSorteado(int numeroSorteado) {
+        this.numeroSorteado = numeroSorteado;
+    }
+}
